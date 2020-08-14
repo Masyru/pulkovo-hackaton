e19718ab5af3b0254bd403f4fd59920aec6cf35c
vanvanich9 <57123154+vanvanich9@users.noreply.github.com>
2020-08-15 08:39:22 +1000

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, redirect, abort, url_for
+import os
 
 app = Flask(__name__, static_folder="./frontend", template_folder="./frontend")
+
+admin_login = "admin"
+admin_password = "gPZENmQb6v2vlLY1"
 
 @app.route("/", methods=["POST", "GET"])
 @app.route("/index", methods=["POST", "GET"])
@@ -9,6 +13,8 @@ def index():
 
 @app.route("/admin", methods=["POST", "GET"])
 def admin():
+    if request.method == "POST":
+        pass
     return render_template("Source/admin.html")
 
 if __name__ == '__main__':
