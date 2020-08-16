2adadf6cee0e6982f6a79c580bbb59e02e653c03
Maksim Kr <mimoz200385@gmail.com>
2020-08-17 00:35:07 +1000

    Added and upgrade __init__

--- __init__.py
+++ __init__.py
@@ -347,4 +347,5 @@ def admin():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # app.run(host="127.0.0.1", port=8080, debug=True)
