682eaabb005057e4418783d1263409a6fecc41c2
Masyru <mimoz200385@gmail.com>
2020-08-16 20:10:19 +1000

    Added admin app

--- frontend/components/Admin.js
+++ frontend/components/Admin.js
@@ -1,8 +1,35 @@
 import React from "react";
+import "../assets/style/App.css";
 import "../assets/style/Admin.css";
+import Header from "./Header";
+import Content from "./Content";
+import Dialog from "./Schedule";
+import { Provider, Form, TextField } from "@adobe/react-spectrum";
 
 export default class extends React.Component{
     render(){
-        return(<div>Hello admin!</div>)
+        let login =
+            <Provider colorScheme="light" className={'login-react'}>
+                <Form maxWidth="size-3600">
+                  <TextField label="Email" placeholder="abc@adobe.com" />
+                  <TextField label="Password" placeholder="1234" />
+                </Form>
+            </Provider>
+
+
+        let admin =
+            <>
+                <Header changeSchedule={this.changeSchedule} />
+                <hr className={'first-hr my-4'}/>
+                <Content data={this.state.data} mode={this.state.current_window} showSchedule={this.showSchedule}/>
+                <hr className={'first-hr my-4'}/>
+                <Dialog
+                    show={this.state.schedule.subjects !== undefined && this.state.schedule.subjects !== null && this.state.schedule.subjects.length}
+                    schedule={this.state.schedule.subjects}
+                    onHide={() => this.showSchedule([])}
+                />
+            </>
+
+        return(login)
     }
 }
